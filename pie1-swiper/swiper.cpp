95f3ee0e94680b88eb723d47aefdf7053230b7b6
Swifty-Sousa <chso8299@colorado.edu>
2019-10-31 21:59:00 -0600

    swiper update 1.0.1

--- pie1-swiper/swiper.cpp
+++ pie1-swiper/swiper.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -7,56 +9,51 @@ void start_swiper()
     while(true) 
     {
         string input;
-        cout<<"Would you like to swipe card or enter ID"<< endl;
+        string holder;
+        string student_id;
+        string student_name;
+        string student_card;
+        cout<<"Please swipe your student ID now:"<<endl;
         cin>>input;
-        if(input=="swipe")
+        if(input=="q")
         {
-            cout<<"Please swipe card now">>endl;
-            cin>> input;
-            if(!fetch(input))
-            {
-                string des;
-                cout<<"Card not associated with account, Do you have an account? (Y/N)"<<endl;
-                cin>>des;
-                des= tolower(des);
-                if(des=="y")
-                {
-                    string user;
-                    string pass;
-                    cout<<"Enter Username (Identikey@colorado.edu):"<<endl;
-                    cin>>user;
-                    cout>>"Enter your [PRODUCT NAME HERE] password:"<<endl;
-                    cin>>pass;
-                    associate(des, user, pass)
-                }
-            }
-            else
-            {
-                // account exsists and is associated.
-                // add event to students event data.
-            }
+            break;
         }
-        else
-        {
-            cout<<"Do you have an account with us"
-        }
+        stringstream ss(input);
+        getline(ss,holder, '=');
+        student_card= holder.erase(0,2);
+        getline(ss,holder, '=');
+        student_id= holder;
+        getline(ss,holder, '=');
+        string holder2;
+        stringstream iss(holder);
+        getline(iss, holder2,'/');
+        student_name= holder2;
+        getline(iss, holder2, '/');
+        student_name= student_name + " " + holder2;
+        cout<< endl;
+        cout<< endl;
+        cout<< endl;
+        cout<<"Name: "<<student_name<<endl;
+        cout<<"ID: "<<student_id<<endl;
+        cout<<"Card Number: "<< student_card;
+        cout<<endl;
+        cout<< "thank you for swiping, this event will apear in your history within the next 24 hours"<<endl;
+        cout<< endl;
+        cout<< endl;
+        cout<< endl;
+
     }
 
+
 }
+
 void associate(string id, string user, string pass)
 {
     //associate id with account.
 }
-void createaccount()
-{
-    string user;
-    string pass;
-    cout<<"Enter Username (Identikey@colorado.edu):"<<endl;
-    cin>>user;
-    cout>>"Enter your [PRODUCT NAME HERE] password:"<<endl;
-    cin>>pass;
 
-}
+
 
 string fetch(string id)
 {
