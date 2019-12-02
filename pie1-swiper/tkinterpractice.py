b88940500e7ab34073695f383f0239003470ac67
Swifty-Sousa <chso8299@colorado.edu>
2019-12-01 21:05:25 -0700

    more gui testing

--- pie1-swiper/tkinterpractice.py
+++ pie1-swiper/tkinterpractice.py
@@ -1,31 +1,21 @@
 #this is my practice gui
 import tkinter as tk
+from tkinter import font as tkfont
 class Applicaiton(tk.Frame):
     def __init__(self, master= None):
-        tk.Frame.__init__(self,master)
-        self.pack()
-        self.log()
-        self.buff()
-        self.hold()
-    def log(self):
-        self.test= tk.Button(self)
-        self.test["text"]="this is test"
-        self.test["command"]=self.buff
-        self.test.pack(side="top")
-        self.hold= tk.Button(self)
-        self.hold["text"]= "manual"
-        self.hold["command"]=self.hold
-        self.QUIT= tk.Button(self, text="exit", command= root.destroy)
-        self.QUIT.pack(side="bottom")
+        tk.TK.__init__(self)
+        #create a container where we can stack multiple gui pages 
+        #use frame raising to move the current page to the top so that it is visible
+        container= tk.Frame(self)
+        container.pack(side="top", fill= "both", expand= True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames={}
+        for i in (EventLogin, CardLogin, ManualLogin, NewLogin):
 
-    def buff(self):
+
         
-        
-    def hold(self):
-
-    def hellothere(self):
-        print("hello there.")
-
+class Card_login
 root =tk.Tk()
 root["height"]=400
 root["width"]=400
