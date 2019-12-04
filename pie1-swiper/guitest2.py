3cae3a59b63d152f874916e1bfe42c0a53c089d8
Swifty-Sousa <chso8299@colorado.edu>
2019-12-04 16:02:04 -0700

    swapping devices

--- pie1-swiper/guitest2.py
+++ pie1-swiper/guitest2.py
@@ -1,84 +1,206 @@
-#THis is NOT my work, this is based off of this stack overflow article: https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
-# the sole purpose of this is for me to experiment with the tkinter libaray without having to write the whole framework.
-import tkinter as tk                # python 3
-from tkinter import font  as tkfont # python 3
-#import Tkinter as tk     # python 2
-#import tkFont as tkfont  # python 2
+#gui controller for project.
+import tkinter as tk
+from tkinter import font as tkfont
+from logic import *
 
-class SampleApp(tk.Tk):
+holder=["", "", "", ""]
+def read(data=[]):
+    global holder
+    holder=data
+    print(holder)
+class stu():
+    sfname=""
+    slname=""
+    sid=""
+    scnum=""
 
-    def __init__(self,):
+student= stu()
+    
+def clear_student(data=[], *args):
+    global student
+    student.sfname=""
+    student.slname=""
+    student.sid=""
+    student.csnum=""
+
+def create_student(data=[], *args):
+    print("data recived:")
+    print(data)
+    global student
+    student.sfname=data[0]
+    student.slname=data[1]
+    student.sid= data[2]
+    student.csnum=data[3]
+    print("class bs:")
+    print(student.sfname)
+
+
+class Applicaiton(tk.Tk):
+    def __init__(self, master= None):
         tk.Tk.__init__(self)
-
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
+        #create a container where we can stack multiple gui pages 
+        #use frame raising to move the current page to the top so that it is visible
+        self.title_font= tkfont.Font(family= "Helvetica", size=18, weight="bold")
+        container= tk.Frame(self)
+        container.pack(side="top", fill= "both", expand= True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
+        self.frames={}
+        for i in (EventLogin, Selector, CardLogin, CardSubmit, ManualLogin, NewLogin):
+            page_name= i.__name__
+            frame = i(parent=container, controller=self)
+            self.frames[page_name]= frame
             frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame("EventLogin")
 
-        self.show_frame("StartPage")
-
+    #brings frame to the front of app to be interacted with
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
 
 
-class StartPage(tk.Frame):
 
+class EventLogin(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        controller= controller
+        #def create(self):
+        #lables and titles and the such
+        label= tk.Label(self, text= "Please Enter Event ID to activate Swiper", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
+        #input Code:
+        eventid= tk.StringVar(self)
+        EventId= tk.Entry(self, textvariable= eventid) 
+        EventId.pack()
+        #buttons to go next:
+        b1= tk.Button(self, text="Next", command= lambda: [controller.show_frame("Selector"), eventid.set(EventId.get()), Ver_Event(eventid.get()), EventId.delete(0,'end')])
+        b1.pack()
+           
 
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.show_frame("PageTwo"))
-        button1.pack()
-        button2.pack()
+
+class Selector(tk.Frame):
+    def __init__(self,parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller= controller
+        #labels:
+        lable= tk.Label(self, text="Please Select how you would like to Sign in:", font= controller.title_font)
+        lable.pack(side="top", fill="x", pady=10)
+        #buttonies
+        card= tk.Button(self, text="Sign in with BuffOne card", command= lambda: controller.show_frame("CardLogin"))
+        man= tk.Button(self, text="Sign in with Student ID number", command= lambda: controller.show_frame("ManualLogin"))
+        new= tk.Button(self, text="Don't have an account", command= lambda: controller.show_frame("NewLogin"))
+        back= tk.Button(self, text="Back", command= lambda: [controller.show_frame("EventLogin")])
+        card.pack()
+        man.pack()
+        new.pack()
+        back.pack()
 
 
-class PageOne(tk.Frame):
+class CardLogin(tk.Frame):
+    def __init__(self,parent, controller):
+        tk.Frame.__init__(self,parent)
+        self.controller= controller
+        self.boi()
+    def boi(self):
+        #Labels:
+        self.lable= tk.Label(self, text="Please Swipe Card", font=self.controller.title_font)
+        self.lable.pack(side="top", fill="x", pady=10)
+        #Entry for card to be swiped:
+        data= tk.StringVar()
+        Data= tk.Entry(self, textvariable=data) 
+        Data.pack()
+        self.b1= tk.Button(self, text="Submit", command= lambda: [print("step1"),data.set(Data.get()),create_student(parse_card(data.get())), Data.delete(0,'end'), self.controller.show_frame("CardSubmit")])
+        self.b1.pack()
+        
 
+        #button to go back to selector:
+        self.menu= tk.Button(self, text="Menu", command= lambda: [self.controller.show_frame("Selector")])
+        self.menu.pack()
+
+class CardSubmit(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        self.controller= controller
+        global student
+        global holder
+        print("this is in card submit")
+        print(student.sfname)
+        var = tk.StringVar(self)
+        var2= tk.StringVar(self)
+        var3= tk.StringVar(self)
+        self.l= tk.Label(self, textvariable=var)
+        l2= tk.Label(self, textvariable=var2)
+        l3= tk.Label(self, textvariable=var3)
+        self.l.pack()
+        l2.pack()
+        l3.pack()
+        var.set("Student Name:"+ holder[0] + " "+ holder[1]+ '\n')
+        var2.set("Student_ID: " + student.sid + '\n')
+        var3.set("Card number: "+ student.scnum + '\n')
+        CardSubmit2.boi()
+
+class CardSubmit2(CardSubmit):
+    @staticmethod
+    def __init__(self):
+        tk.Frame.__init__(self)
+    @staticmethod
+    def boi():
+        print("please fucking work rreeeeee")
+'''put a holder updater in the submit of cardlogin, have it go update the holder array, then call an updater for cardsubmit
+'''
+
+def pront(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+
+class ManualLogin(tk.Frame):
+    def __init__(self,parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller= controller
+        #button to go back to selector:
+        menu= tk.Button(self, text="Menu", command=lambda: controller.show_frame("Selector"))
+        #lables:
+        label_id= tk.Label(self, text="Student ID:")
+        label_fname= tk.Label(self, text="First Name:")
+        label_lname= tk.Label(self, text="Last Name:")
+        #Entry input variables:
+        input_id= tk.StringVar()
+        input_fname= tk.StringVar()
+        input_lname= tk.StringVar()
+        #Entries:
+        in_id= tk.Entry(self, textvariable=input_id)
+        in_fname= tk.Entry(self, textvariable=input_fname)
+        in_lname= tk.Entry(self, textvariable=input_lname)
+        #packing:
+        label_fname.pack()
+        in_fname.pack()
+        label_lname.pack()
+        in_lname.pack()
+        label_id.pack()
+        in_id.pack()
+        submit= tk.Button(self, text="Submit", command= lambda:pront(input_fname.get(),input_lname.get(), input_id.get()))
+        submit.pack()
+        
 
 
-class PageTwo(tk.Frame):
+        menu.pack()
 
+class NewLogin(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        self.controller= controller
+        #Das lable:
+        label =tk.Label(self, text="Please Visit our website at: WEBSITE URL HERE DONT FORGET THIS U FUCK to create an account", font=controller.title_font )
+        label.pack()
+        #button to go back to selector:
+        menu= tk.Button(self, text="Menu", command= lambda: controller.show_frame("Selector"))
+        menu.pack()
 
 
-if __name__ == "__main__":
-    app = SampleApp()
+if __name__=="__main__":
+    #event= Applicaiton().frames["EventLogin"]
+    app=Applicaiton()
+    app.frames["CardSubmit"].l['text']="puta"
+    app.frames["CardSubmit"].l.pack()
     app.mainloop()
--- pie1-swiper/test.py
+++ pie1-swiper/test.py
@@ -1,24 +1,46 @@
-import tkinter as tk
+from tkinter import *
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+class First(Frame):
+    def __init__(self,master):
+        super().__init__()
+        master.minsize(width=755, height=520)
+        master.maxsize(width=755, height=520)
+        Grid.config(self)
 
-    def createWidgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        # Run all function in [First Class]
+        self.widget_size()
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                                            command=root.destroy)
-        self.QUIT.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def widget_size(self):
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+        # Define a small frame for it
+        self.main_inner_frame = LabelFrame(self,text="Tracing Method",height= 120,width =355)
+        self.main_inner_frame.grid(row= 0, column=0)
+        self.main_inner_frame.grid_propagate(0)
+
+        # Create a entry box for the user
+        # use a string variable tvar
+        tvar = StringVar()
+        self.traceEntry = Entry(self.main_inner_frame,textvariable=tvar,width=30)
+        self.traceEntry.grid(row = 0, column = 2, sticky = W)
+
+        # Create a button for it [When trace click it will show the text in the entry on terminal]
+        self.traceButton = Button(self.main_inner_frame, text="Trace",command=lambda: Second.printSecondLine(tvar))
+        self.traceButton.grid(row = 0, column = 1, sticky = W)
+
+
+class Second(First):
+    @staticmethod
+    def __init__(self,master):
+        super().__init__(master)
+
+    @staticmethod
+    def printSecondLine(value):
+        print(value.get())
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Good System")
+    TIF = First(root)
+    root.mainloop()
